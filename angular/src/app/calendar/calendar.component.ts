1cb1fcb9246b1bc447eec995e49a0ff317f08420
dtorney <dtorney@vt.edu>
2020-05-08 17:38:18 -0400

    Fix calendar display and dislay user's current courses

--- angular/src/app/calendar/calendar.component.ts
+++ angular/src/app/calendar/calendar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {CourseService} from "../_services/course.service";
+import {NotificationService} from "../_services/notification.service";
+import {Course} from "../_models/course";
+import {AuthService} from "../_services/auth.service";
 
 @Component({
   selector: 'app-calendar',
@@ -6,21 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-  currentMonday = this.getMonday(new Date(), 0);
-  currentTuesday = this.getMonday(new Date(), 1);
-  currentWednesday = this.getMonday(new Date(), 2);
-  currentThursday = this.getMonday(new Date(), 3);
-  currentFriday = this.getMonday(new Date(), 4);
-  constructor() { }
+  currentWeek: Date[] = [];
+  courses: Course[] = [];
+  username: string;
 
-  ngOnInit() {
+  constructor(private courseService: CourseService, private notifService: NotificationService, private authService: AuthService) {
   }
 
-  getMonday(d, x) {
+  ngOnInit() {
+    this.username = this.authService.currentUserValue.username;
+    this.loadAllCourses(this.username);
+    this.getCurrentWeek(new Date());
+  }
+
+  private loadAllCourses(username: string) {
+    this.courseService.getAll().subscribe(
+      courses => {
+        this.courses = courses.filter(function (course) {
+          return course.username === username;
+        });
+        console.log(this.courses);
+
+      },
+      error => {
+        this.notifService.showNotif(error.toString(), 'warning');
+      });
+  }
+
+
+  private getMonday(d, x) {
     d = new Date(d);
     let day = d.getDay(),
       diff = d.getDate() - day + (day == 0 ? -6:1);
     return new Date(d.setDate(diff + x));
+  }
+
+  private getCurrentWeek(d) {
+    let i;
+    for (i = 0; i < 5; i++) {
+      this.currentWeek[i] = this.getMonday(d, i);
+    }
   }
 
 
