dc5f0d9f34f6a8f4e73255ece11b077ecf62024b
andrewperry <andrewperry@vt.edu>
2020-05-07 20:12:06 -0400

    Finished HTML/CSS for assignment

--- angular/src/app/add-assignment/add-assignment.component.ts
+++ angular/src/app/add-assignment/add-assignment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {first} from 'rxjs/operators';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-add-assignment',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-assignment.component.css']
 })
 export class AddAssignmentComponent implements OnInit {
-
+  date: FormControl = new FormControl({ value: new Date(), disabled: false});
+  addDate: Date = new Date();
+  addCourse: 'CS3754';
+  buttonName = 'Add';
   constructor() { }
 
   ngOnInit() {
   }
 
+  createAssignment() {
+    // Use UserService to add the assignment
+  }
 }
